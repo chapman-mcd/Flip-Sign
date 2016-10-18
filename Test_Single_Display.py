e76a8a55ad58a2f514ac36fb5d1117a8d30c6539
Kiganshee <chapman.mcdaniel@gmail.com>
2016-10-17 21:33:49 -0500

    Generated test case for single 28x14 display

---
+++ Test_Single_Display.py
@@ -0,0 +1,37 @@
+from MessageClasses import *
+from DisplayClasses import *
+import serial
+
+def Generate_Layout_1():
+    """
+    Generates a layout dictionary for a single 28x14 display, according to the layout specifications.
+    :return:
+    """
+    output = {}
+    #first display
+    for i in range(28):
+        for j in range(7):
+            output[(j, i)] = (1, i, 2**j)
+
+    #second display
+    for i in range(28):
+        for j in range(7):
+            output[(j, i+7)] = (2, i, 2**j)
+    return output
+
+port = '/dev/ttyS0'
+
+serialinterface = serial.Serial(port=port, baudrate=57600, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS,
+                                timeout=1, stopbits=serial.STOPBITS_ONE)
+
+Display = FlipDotDisplay(14, 28, serialinterface=serialinterface, layout=Generate_Layout_1())
+
+while True:
+    test = []
+    test.append(BasicTextMessage('TEST test'))
+    test.append(BasicTextMessage('HELLO WORLD'))
+    test.append(BasicTextMessage('FLIP DOTS'))
+
+    for msg in test:
+        Display.update(SimpleTransition, msg,
+                       font=ImageFont.truetype('/Users/cmcd/PycharmProjects/Sign/PressStart2P.ttf', size=7))
