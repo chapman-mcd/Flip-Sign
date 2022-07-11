483d38be47a25e1774e70f0374d002ee053b254d
Kiganshee <chapman.mcdaniel@gmail.com>
2022-07-11 08:04:34 -0300

    Weather API Helper functions complete w/ tests

--- tests/helpers/weather_api_test.py
+++ tests/helpers/weather_api_test.py
@@ -2,8 +2,9 @@ import flip_sign.helpers
 import time
 from unittest.mock import patch, MagicMock
 from urllib.request import Request
+from urllib.error import HTTPError
 
-
+# Tests the time-to-use (TTU) function for the accuweather cache
 def test_api_cache_ttu():
     # test result for 5-day forecast request
     five_day_url = ('http://dataservice.accuweather.com/forecasts/v1/daily/5day/', '431090', "?apikey=IFDAHGDjkdf",
@@ -18,6 +19,7 @@ def test_api_cache_ttu():
     location_ttu = flip_sign.helpers.accuweather_cache_ttu(_key=location_url, value=None, now=time_now)
     assert location_ttu == time_now + 30 * 24 * 60 * 60 # cache location lookup results for 30 days
 
+# Tests the logging feature for the TTU function
 logging_mock = MagicMock()
 @patch("logging.getLogger", logging_mock)
 def test_api_cache_ttu_logging():
@@ -39,6 +41,7 @@ urlopen_mock = MagicMock()
 req = Request(''.join(test_location_url_tuple), headers={"Accept-Encoding": "gzip, deflate"})
 urlopen_mock(req).read = MagicMock(return_value = urlopen_result)
 
+# Tests that the correct value is returned from caching
 @patch('flip_sign.helpers.urlopen', urlopen_mock)
 def test_api_requests_basic():
     resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
@@ -47,9 +50,35 @@ def test_api_requests_basic():
     resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
     urlopen_mock(req).read.assert_called_once() # mock is only called once due to caching
 
-# test whether exceptions are cached
+# Confirms that the method is called again after cache expires
+@patch('flip_sign.helpers.urlopen', urlopen_mock)
+def test_api_requests_cache_expiry():
+    urlopen_mock(req).read.reset_mock()
+    flip_sign.helpers._accuweather_cache.clear()
+    resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
+    resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
+    urlopen_mock(req).read.assert_called_once()  # mock is only called once due to caching
 
-# test more calls to cache after value has expired
+    flip_sign.helpers._accuweather_cache.expire(time=2e10) # simulate time expiry using the 'expire' method
+    resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
+    assert len(urlopen_mock(req).read.mock_calls) == 2 # confirm mock is called again
+
+
+# Confirm exceptions are not cached
+@patch('flip_sign.helpers.urlopen', urlopen_mock)
+def test_api_requests_cache_exceptions():
+    err = HTTPError(url=''.join(test_location_url_tuple), code=403, msg='Got an error', hdrs=None, fp=None)
+    urlopen_mock(req).read.configure_mock(side_effect=err)
+    flip_sign.helpers._accuweather_cache.clear()
+    try:
+        resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
+    except HTTPError:
+        pass
+
+    # make another request to confirm the exception was not cached
+    urlopen_mock(req).read.configure_mock(side_effect=None)
+    resp = flip_sign.helpers.accuweather_api_request(test_location_url_tuple)
+    assert resp['Key'] == '335689'  # Ensure returns correct result
 
 # HTTP Errors:
 # 400 Bad Request
