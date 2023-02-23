4bbb22aeeaa53a9f05955022adeafe0d4d161393
Kiganshee <chapman.mcdaniel@gmail.com>
2023-02-23 17:05:52 -0300

    Restructured google API client_secret to be a path to the file
    This fits better with the way the get_credentials function in
    the default google Python API works.

--- flip_sign/assets.py
+++ flip_sign/assets.py
@@ -3,10 +3,11 @@ import os
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
-key_names = ['AccuweatherAPI', 'GoogleAPIs', 'GoogleLocationAPI', 'GoogleDriveFolder', 'GoogleSheet']
-key_paths = ['WeatherKey.txt', 'client_secret.json', 'Google_Location_Key.txt',
+key_names = ['AccuweatherAPI', 'GoogleLocationAPI', 'GoogleDriveFolder', 'GoogleSheet']
+key_paths = ['WeatherKey.txt',  'Google_Location_Key.txt',
              'Google_Drive_Folder.txt', 'GoogleSheet.txt']
 keys = {name: Path(root_dir + "/assets/keys/" + path).read_text() for name, path in zip(key_names, key_paths)}
+keys['GOOGLE_CLIENT_SECRET_FILE'] = Path(root_dir + "/assets/keys/client_secret.json")
 
 font_names = ['PressStart2P', 'DatDot']
 font_paths = ['PressStart2P.ttf', 'DatDot_edited_v1.ttf']
