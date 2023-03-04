be807ecbddd6eda42e4bf3ed8873ec7ba1de6e9b
Kiganshee <chapman.mcdaniel@gmail.com>
2023-03-04 09:33:35 -0300

    Added a location for the Google credentials token to be stored

--- flip_sign/assets.py
+++ flip_sign/assets.py
@@ -8,6 +8,7 @@ key_paths = ['WeatherKey.txt',  'Google_Location_Key.txt',
              'Google_Drive_Folder.txt', 'GoogleSheet.txt']
 keys = {name: Path(root_dir + "/assets/keys/" + path).read_text() for name, path in zip(key_names, key_paths)}
 keys['GOOGLE_CLIENT_SECRET_FILE'] = Path(root_dir + "/assets/keys/client_secret.json")
+keys['GOOGLE_TOKEN_PATH'] = Path(root_dir + "/assets/keys/token.json")
 
 font_names = ['PressStart2P', 'DatDot']
 font_paths = ['PressStart2P.ttf', 'DatDot_edited_v1.ttf']
