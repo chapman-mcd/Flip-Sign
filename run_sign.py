6f2cd48660552f2c6076a44316946c8c7810b9e3
Kiganshee <chapman.mcdaniel@gmail.com>
2023-05-19 16:34:27 -0300

    Fixed logging issue in convenience script

--- run_sign.py
+++ run_sign.py
@@ -1,3 +1,7 @@
 from flip_sign.run_sign import run_sign
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.root.handlers = []  # remove initial console handler, will be replaced
 
 run_sign()
